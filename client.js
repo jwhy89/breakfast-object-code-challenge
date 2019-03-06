07b0d80001c2d5479949c43a1ac44a3edd0c087d
Jarvis Yang <jarvisgyang@gmail.com>
2019-03-06 14:07:12 -0600

    removed for loop

--- client.js
+++ client.js
@@ -1,6 +1,6 @@
 let pantryContents  = {
   pancakeMixBags: 1,
-  chocolateChips: 20
+  chocolateChips: 20,
 };
 
 let refrigeratorContents  = {
@@ -14,18 +14,6 @@ let refrigeratorContents  = {
 
 // You shouldn't need to change anything above this line initially
 // You may change it to test different scenarios, but it should work as is
-
-function breakfastChecker(contents) {
-  // Your code goes here
-  for ( let checker of contents ) {
-  // Create if conditional to look through values
-  if ( eaters * mealTwo.chocChipPancake >= ( checker.pancakeMixBags / pancakeBatch ) * ( checker.chocolateChips / pancake ) ) {
-    return true;
-
-
-  } else {
-    return false;}// end for loop
-} // end breakfastChecker function
 
 // figure out how much of ingredients are need per person
 let mealOne = {
@@ -43,6 +31,25 @@ let pancakeBatch = 10; // number of pancakes per mix
 let pancake = 15; // number of choc chips per pancake
 
 let eaters = 4;
+
+// breakfast checker function
+function breakfastChecker(contents) {
+  // Your code goes here
+//  for ( let checker of contents ) {
+  let checker = contents
+  // Create if conditional to look through values
+  if ( eaters * mealTwo.chocChipPancake >= ( checker.pancakeMixBags * pancakeBatch ) * ( checker.chocolateChips / pancake ) ) {
+    return true;
+  }
+  else if (0 >= (checker.pancakeMixBags / pancakeBatch) * (checker.chocolateChips / pancake)) {
+
+
+  } else {
+    return false;}
+} // end for loop
+//}  end breakfastChecker function
+
+
 
 // Your function should return 'yes' or 'no'
 console.log('Does the pantry have enough food for breakfast?', breakfastChecker(pantryContents));
